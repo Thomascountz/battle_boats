b572d009ead5f80e5f3ce7363be372f69616eda1
Thomas Countz <thomascountz@gmail.com>
2018-05-11 15:33:38 -0400

    implement console interface functionality

--- lib/battle_boats/console_ui.rb
+++ lib/battle_boats/console_ui.rb
@@ -1,31 +1,75 @@
 module BattleBoats
-  class ConsoleUI
-    def initialize(output: $stdout)
-      @output = output
-    end
+	class ConsoleUI
+		def initialize(output: $stdout, input: $stdin)
+			@output = output
+			@input = input
+		end
 
-    def greet
-      output.puts "Welcome to Battle Boats!"
-    end
+		def greet
+			output.puts "Welcome to Battle Boats!"
+		end
 
-    def display_board(board)
-      raise NotImplementedError
-    end
+		def display_board(board)
+			output.puts format_board(board)
+		end
 
-    def get_row
-      raise NotImplementedError
-    end
+		def get_row
+			output.puts "Target row:"
+			input.gets.chomp
+		end
 
-    def get_column
-      raise NotImplementedError
-    end
+		def get_column
+			output.puts "Target column:"
+			input.gets.chomp
+		end
 
-    def display_status_report(status_report)
-      raise NotImplementedError
-    end
+		def display_status_report(status_report)
+			output.puts status_report
+		end
 
-    private
+		private
 
-    attr_reader :output
-  end
+		attr_reader :output, :input
+
+		def format_board(board)
+			board_string = horizontal_line
+			board_string << newline
+			board_string << top_row
+			board_string << horizontal_line
+			board_string << newline
+			board.play_area.each_with_index do |row, row_number|
+				board_string << pipe
+				board_string << "  #{row_number}  "
+				board_string << pipe
+				row.each do |cell|
+					if cell
+						board_string << "  #{cell}  "
+					else
+						board_string << "  .  "
+					end
+					board_string << pipe
+				end
+				board_string << newline
+				board_string << horizontal_line
+				board_string << newline
+			end
+			board_string
+		end
+
+		def top_row
+			"|     |  0  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |\n"
+		end
+
+		def newline
+			"\n"
+		end
+
+		def horizontal_line
+			"-" * 67
+		end
+
+		def pipe
+			"|"
+		end
+	end
 end
--- spec/console_ui_spec.rb
+++ spec/console_ui_spec.rb
@@ -1,16 +1,64 @@
 require 'battle_boats/console_ui'
+require 'battle_boats/board'
 
 RSpec.describe BattleBoats::ConsoleUI do
 
   let(:output) { StringIO.new }
-
   subject(:console_ui) { described_class.new(output: output) }
 
   describe '#greet' do
-    it 'prints a welcome message to the output' do
+    it 'prints a greeting to output' do
       console_ui.greet
-      expect(output.string).to include('Welcome', 'Battle Boats')
+
+      expect(output.string.downcase).to include("welcome", "battle boats")
     end
   end
 
+  describe '#display_board' do
+    context 'when initialized' do
+      it 'prints the board to output' do
+        board = BattleBoats::Board.new
+        console_ui.display_board(board)
+
+        expected = "-------------------------------------------------------------------\n|     |  0  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |\n-------------------------------------------------------------------\n|  0  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  1  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  2  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  3  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  4  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  5  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  6  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  7  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  8  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n|  9  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |  .  |\n-------------------------------------------------------------------\n"
+
+        expect(output.string).to eq expected
+      end
+    end
+  end
+
+  describe '#get_row' do
+    it 'returns input received from the player' do
+      input = StringIO.new("4\n")
+      console_ui = BattleBoats::ConsoleUI.new(output: output, input: input)
+
+      result = console_ui.get_row
+
+      expect(output.string).to include("row")
+      expect(result).to eq('4')
+    end
+  end
+
+  describe '#get_column' do
+    it 'returns input received from the player' do
+      input = StringIO.new("3\n")
+      console_ui = BattleBoats::ConsoleUI.new(output: output, input: input)
+
+      result = console_ui.get_column
+
+      expect(output.string).to include("column")
+      expect(result).to eq('3')
+    end
+  end
+
+  describe '#display_status_report' do
+    it 'displays the given status report to the output' do
+      status_report = "STATUS REPORT"
+
+      console_ui.display_status_report(status_report)
+
+      expect(output.string).to include(status_report)
+    end
+  end
 end
+
