00f848d348e45f93f9cd32dd86c57bd33773604e
Thomas Countz <thomascountz@gmail.com>
2018-05-11 15:34:54 -0400

    [rubocop] fix indentation

--- lib/battle_boats/board.rb
+++ lib/battle_boats/board.rb
@@ -1,23 +1,22 @@
 module BattleBoats
-	class Board
-		attr_reader :play_area, :status_report
+  class Board
+    attr_reader :play_area, :status_report
 
-		def initialize
-			@status_report = ""
-			@play_area = []
-			10.times do
-				row = []
-				10.times do
-					row << nil
-				end
-				@play_area << row
-			end
-		end
+    def initialize
+      @status_report = ""
+      @play_area = []
+      10.times do
+        row = []
+        10.times do
+          row << nil
+        end
+        @play_area << row
+      end
+    end
 
-		def strike_position(row:, column:)
-			@play_area[row.to_i][column.to_i] = "X"
-			@status_report = "Miss!"
-		end
-
-	end
+    def strike_position(row:, column:)
+      @play_area[row.to_i][column.to_i] = "X"
+      @status_report = "Miss!"
+    end
+  end
 end
