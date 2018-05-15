76bef501c319ee8b758a11a006b47a7281929207
Thomas Countz <thomascountz@gmail.com>
2018-05-15 10:27:03 -0400

    implement cell class

--- lib/battle_boats/cell.rb
+++ lib/battle_boats/cell.rb
@@ -1,7 +1,25 @@
 module BattleBoats
   class Cell
+    def initialize
+      @hit = false
+    end
+
+    def hit?
+      @hit
+    end
+
+    def strike
+      if !hit?
+        @hit = true
+      end
+    end
+
     def to_s
-      "."
+      if hit?
+        "X"
+      else
+        "."
+      end
     end
   end
 end
--- spec/battle_boats/cell_spec.rb
+++ spec/battle_boats/cell_spec.rb
@@ -2,12 +2,45 @@ require 'battle_boats/cell'
 
 RSpec.describe BattleBoats::Cell do
 
-  subject(:cell) { described_class.new }
+  context 'when a new cell is initialized' do
+    describe '#hit?' do
+      it 'returns false by default' do
+        cell = BattleBoats::Cell.new
+        expect(cell.hit?).to be false
+      end
+    end
+  end
+
+  describe '#stike' do
+    context 'when a cell is not hit' do
+      it 'sets hit? to true' do
+        cell = BattleBoats::Cell.new
+        cell.strike
+        expect(cell.hit?).to be true
+      end
+    end
+    context 'when a cell has already been hit' do
+      it 'returns nil' do
+        cell = BattleBoats::Cell.new
+        cell.strike
+        expect(cell.strike).to be nil
+      end
+    end
+  end
 
   describe '#to_s' do
-      context 'when the cell is unhit' do
-        it 'returns the string representation of an unhit cell' do
-          expect(cell.to_s).to eq(".")
+    context 'when a cell is not hit' do
+      it "returns the string representation of an un-hit cell" do
+        cell = BattleBoats::Cell.new
+        expect(cell.to_s).to eq "."
+      end
+    end
+
+    context 'when a cell has been struck' do
+      it "returns the string representation of a hit cell" do
+        cell = BattleBoats::Cell.new
+        cell.strike
+        expect(cell.to_s).to eq "X"
       end
     end
   end
