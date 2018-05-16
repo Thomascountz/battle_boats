1533b80f5a3ad35fd5ed6ef028ca81de4cc08e7e
Thomas Countz <thomascountz@gmail.com>
2018-05-16 14:22:59 -0400

    create fleet constant

---
+++ lib/battle_boats/fleet.rb
@@ -0,0 +1,9 @@
+require_relative "ship"
+
+module BattleBoats
+  FLEET = [BattleBoats::Ship.new(name: "Carrier",    length: 5, symbol: "C"),
+           BattleBoats::Ship.new(name: "Battleship", length: 4, symbol: "B"),
+           BattleBoats::Ship.new(name: "Cruiser",    length: 3, symbol: "R"),
+           BattleBoats::Ship.new(name: "Submarine",  length: 3, symbol: "S"),
+           BattleBoats::Ship.new(name: "Destroyer",  length: 2, symbol: "D")].freeze
+end
