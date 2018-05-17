6f067532141fdee6e438fd617ab1494f881c01ad
Thomas Countz <thomascountz@gmail.com>
2018-05-17 16:42:07 -0400

    fix typo

--- lib/battle_boats/coordinate.rb
+++ lib/battle_boats/coordinate.rb
@@ -2,7 +2,7 @@ module BattleBoats
   class Coordinate
     attr_reader :row, :column
 
-    def initialize(row: nil, column: column)
+    def initialize(row: nil, column: nil)
       @row = row
       @column = column
     end
