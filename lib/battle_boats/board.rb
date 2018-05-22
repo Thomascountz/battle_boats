306d217c41e6f6121f08b9b841ee4a0552ac9ad1
Thomas Countz <thomascountz@gmail.com>
2018-05-22 10:12:27 -0400

    clean up board#cell_at

--- lib/battle_boats/board.rb
+++ lib/battle_boats/board.rb
@@ -34,7 +34,7 @@ module BattleBoats
     end
 
     def cell_at(coordinate:)
-      if within_range?(coordinate.row) && within_range?(coordinate.column)
+      if within_range?(coordinate)
         @play_area[coordinate.row.to_i][coordinate.column.to_i]
       end
     end
@@ -64,8 +64,8 @@ module BattleBoats
       end
     end
 
-    def within_range?(input)
-      if input.to_s =~ /^[0-9]$/
+    def within_range?(coordinate)
+      if coordinate.row.to_s =~ /^[0-9]$/ && coordinate.column.to_s =~ /^[0-9]$/
         true
       else
         false
