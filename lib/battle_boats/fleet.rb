57583689fbbf58e1579584c31504d45491dc831d
Thomas Countz <thomascountz@gmail.com>
2018-05-23 14:10:56 -0400

    rubocop

--- lib/battle_boats/fleet.rb
+++ lib/battle_boats/fleet.rb
@@ -5,11 +5,13 @@ module BattleBoats
     attr_reader :ships
 
     def initialize
-      @ships = [BattleBoats::Ship.new(name: "Carrier", length: 5, symbol: "C"),
-                BattleBoats::Ship.new(name: "Battleship", length: 4, symbol: "B"),
-                BattleBoats::Ship.new(name: "Cruiser",    length: 3, symbol: "R"),
-                BattleBoats::Ship.new(name: "Submarine",  length: 3, symbol: "S"),
-                BattleBoats::Ship.new(name: "Destroyer",  length: 2, symbol: "D")]
+      @ships = [
+        BattleBoats::Ship.new(name: "Carrier", length: 5, symbol: "C"),
+        BattleBoats::Ship.new(name: "Battleship", length: 4, symbol: "B"),
+        BattleBoats::Ship.new(name: "Cruiser",    length: 3, symbol: "R"),
+        BattleBoats::Ship.new(name: "Submarine",  length: 3, symbol: "S"),
+        BattleBoats::Ship.new(name: "Destroyer",  length: 2, symbol: "D"),
+      ]
     end
   end
 
