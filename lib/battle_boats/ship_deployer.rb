96e46173b1d5f5678943759decf82d00dad8f3a0
Thomas Countz <thomascountz@gmail.com>
2018-05-25 12:20:34 -0400

    move fleet to fleet deployer

--- lib/battle_boats/ship_deployer.rb
+++ lib/battle_boats/ship_deployer.rb
@@ -1,18 +1,36 @@
 module BattleBoats
   class ShipDeployer
-    def initialize(board: BattleBoats::Board.new, interface: BattleBoats::DevConsoleUI.new)
+    def initialize(board: BattleBoats::Board.new,
+                   interface: BattleBoats::DevConsoleUI.new,
+                   fleet: BattleBoats::Fleet.new)
       @board = board
-      @ships = board.fleet.ships
+      @ships = fleet.ships
       @interface = interface
     end
 
-    def place_ships
+    def place_ships_randomly
+      @ships.each do |ship|
+        coin_flip = ["heads", "tails"].sample
+        if coin_flip == "heads"
+          until @board.place_ship_horizontally(coordinate: get_random_coordinate, ship: ship)
+          end
+        else
+          until @board.place_ship_vertically(coordinate: get_random_coordinate, ship: ship)
+          end
+        end
+      end
+      @board
+    end
+
+    def place_ships_manually
       @ships.each do |ship|
         place_ship(ship)
       end
       @interface.display_board(@board)
       @board
     end
+
+    private
 
     def place_ship(ship)
       @interface.display_board(@board)
@@ -45,6 +63,10 @@ module BattleBoats
     def failure(ship)
       puts "FAIL"
       place_ship(ship)
+    end
+
+    def get_random_coordinate
+      BattleBoats::Coordinate.random(row: 0..9, column: 0..9)
     end
   end
 end
